5c57047dc3465864620089e7c26f1c2659662317
Gokarna Adhikari <agokarna444@gmail.com>
2024-05-03 10:55:17 +0545

    complete the video detail section

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, VideoDetailsView, VideoListView
 app_name = 'home'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('video/', VideoListView.as_view(), name='video'),
+    path('video/detail/', VideoDetailsView.as_view(), name='detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,11 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = "home/index.html"
     
+
+class VideoListView(TemplateView):
+    template_name = "videos/video.html"
+
+
+# Details page of video
+class VideoDetailsView(TemplateView):
+    template_name = "videos/detail.html"
